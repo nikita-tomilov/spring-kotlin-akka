1e6230a25a337fb511b7349afe137134021ececc
Nikita Tomilov <programmer174@icloud.com>
2024-04-07 12:11:33 +0200

    FeignBlockingGithubClient: fix typing

--- src/main/kotlin/ru/ifmo/springkotlinakka/client/FeignBlockingGithubClient.kt
+++ src/main/kotlin/ru/ifmo/springkotlinakka/client/FeignBlockingGithubClient.kt
@@ -3,10 +3,11 @@ package ru.ifmo.springkotlinakka.client
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import ru.ifmo.springkotlinakka.model.GithubRepo
 
 @FeignClient(value = "github", url = "https://api.github.com")
 interface FeignBlockingGithubClient {
 
   @GetMapping(path = ["/users/{username}/repos"])
-  fun fetchRepositories(@PathVariable username: String): List<Any>
+  fun fetchRepositories(@PathVariable username: String): List<GithubRepo>
 }
